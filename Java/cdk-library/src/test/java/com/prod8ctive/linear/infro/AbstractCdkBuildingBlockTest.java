e1c65e6175d375e6e22857abb4834584aaac4262
Lalit C <prod8ctive.analytics@gmail.com>
2022-02-14 03:29:59 -0600

    JUnit testcases using CDK test module

---
+++ Java/cdk-library/src/test/java/com/prod8ctive/linear/infro/AbstractCdkBuildingBlockTest.java
@@ -0,0 +1,114 @@
+package com.prod8ctive.infro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import com.prod8ctive.infra.common.AbstractCdkBuildingBlock;
+
+import software.amazon.awscdk.Environment;
+import software.amazon.awssdk.regions.Region;
+
+/**
+ * Test case for utility methods from Abstract Super Class
+ * 
+ * @author chawl001
+ */
+class AbstractCdkBuildingBlockTest {
+	
+	static String TEST_REGION 	= Region.US_WEST_2.id();
+	static String TEST_ACCOUNT 	= "887847050650";
+	
+	static String ENV_REGION	= Region.EU_CENTRAL_1.id();
+	static String ENV_ACCOUNT	= "123456789000";
+	
+	static String S3_BUCKET = "laap-ue1-gen-repository-sbx";
+	static String TOPICS_FOLDER_WITH_DELIMETER = "avro_2_glue/output/";
+	static String TOPIC_NAME = 
+//			"lndcdc_ncs_tcs__flight_dates" 	/* -- GoldenGate topics */ 
+			"lndcdcncstcs__flightdates" 	/* -- NiFi topics */ ;
+	
+	static final String TEST_MDOULE_NAME = "concust";
+
+	@Test
+	@Disabled ("Need to Mock CDK environment")
+	void testMakeEnv() {
+		Environment testEnvironment = AbstractCdkBuildingBlock.makeEnv(TEST_REGION, TEST_ACCOUNT);
+		assertNotNull(testEnvironment, "Expected non-null CDK env");
+		assertEquals(TEST_REGION, testEnvironment.getRegion());
+		assertEquals(TEST_ACCOUNT, testEnvironment.getAccount());
+		
+		System.setProperty("CDK_DEFAULT_ACCOUNT", "dummy-accnt");
+		System.setProperty("CDK_DEFAULT_REGION", null);
+		try {
+			Environment nullEnv = AbstractCdkBuildingBlock.makeEnv(null, null);
+			fail("Should fail for setting null value for CDK_DEFAULT_REGION");
+		} catch (AssertionError ae) {
+			System.out.println("Don't worry...Expected to fail for null CDK_DEFAULT_REGION with message: "+ ae.getMessage());
+		}
+		
+		System.setProperty("CDK_DEFAULT_ACCOUNT", ENV_ACCOUNT);
+		System.setProperty("CDK_DEFAULT_REGION", "my-planet-1");
+		try {
+			Environment invalidEnvironment = AbstractCdkBuildingBlock.makeEnv(null, null);
+			fail("Should fail for setting \"my-planet-1\" value for CDK_DEFAULT_REGION");
+		} catch (AssertionError ae) {
+			System.out.println("Don't worry...Expected to fail for \"my-planet-1\" CDK_DEFAULT_REGION with message: "+ ae.getMessage());
+		}
+		
+		System.setProperty("CDK_DEFAULT_ACCOUNT", ENV_ACCOUNT);
+		System.setProperty("CDK_DEFAULT_REGION", ENV_REGION);
+		Environment envTestEnvironment = AbstractCdkBuildingBlock.makeEnv(null, null);
+		assertNotNull(testEnvironment, "Expected non-null CDK env");
+		
+		assertEquals(ENV_REGION, envTestEnvironment.getRegion());
+		assertEquals(ENV_ACCOUNT, envTestEnvironment.getAccount());
+
+	}
+	
+	@Test
+	void testExtraction() {
+		// build test paths
+		String GLUE_FILE_NAME = String.format("%s.glue.csv", TOPIC_NAME);
+		String TEST_S3_PATH = String.format("%s%s", TOPICS_FOLDER_WITH_DELIMETER, GLUE_FILE_NAME);
+		
+		System.out.println("Testing S3 path: "+ TEST_S3_PATH);
+		
+		String extractedGlueFilename = AbstractCdkBuildingBlock.extractGlueSchemaFile(TEST_S3_PATH);
+		assertEquals(GLUE_FILE_NAME, extractedGlueFilename, "Extraction of Glue Schema file name must match");
+		
+		String extractedKafkaTopicname = AbstractCdkBuildingBlock.extractKafkaTopic(GLUE_FILE_NAME);
+		assertEquals(TOPIC_NAME, extractedKafkaTopicname, "Extraction of Kafka Topic name must match");
+	}
+	
+	@Test
+	void testGetBaseFolder() {
+		final boolean ERROR_FOLDER = true;
+		
+		// Landing
+		final String landingTopic = "tcs__agency_address";
+		String landingBaseFolder = AbstractCdkBuildingBlock.getBaseFolder(TEST_MDOULE_NAME, (! ERROR_FOLDER), landingTopic);
+		assertNotNull(landingBaseFolder);
+		assertFalse(landingBaseFolder.contains(TEST_MDOULE_NAME), "Landing s3 folder path must NOT include module_name: "+ TEST_MDOULE_NAME);
+		assertEquals("landing-topics/"+ landingTopic + "/", landingBaseFolder);
+		
+		String landingBaseErrorFolder = AbstractCdkBuildingBlock.getBaseFolder(TEST_MDOULE_NAME, ERROR_FOLDER, landingTopic);
+		assertNotNull(landingBaseErrorFolder);
+		assertFalse(landingBaseErrorFolder.contains(TEST_MDOULE_NAME), "Landing s3 Error folder path must NOT include module_name: "+ TEST_MDOULE_NAME);
+		assertEquals("landing-topics/error_records/"+ landingTopic + "/", landingBaseErrorFolder);
+		
+		// Conformance
+		final String conformanceTopic = "conformance_target_concust__agcy";
+		String conformanceBaseFolder = AbstractCdkBuildingBlock.getBaseFolder(TEST_MDOULE_NAME, (! ERROR_FOLDER), conformanceTopic);
+		assertNotNull(conformanceBaseFolder);
+		assertTrue(conformanceBaseFolder.contains(TEST_MDOULE_NAME), "Conformance s3 folder path MUST include module_name: "+ TEST_MDOULE_NAME);
+		assertEquals("conformance-topics/"+ TEST_MDOULE_NAME + "/" + conformanceTopic + "/", conformanceBaseFolder);
+		
+		String conformanceBaseErrorFolder = AbstractCdkBuildingBlock.getBaseFolder(TEST_MDOULE_NAME, ERROR_FOLDER, conformanceTopic);
+		assertNotNull(conformanceBaseErrorFolder);
+		assertTrue(conformanceBaseErrorFolder.contains(TEST_MDOULE_NAME), "Conformance s3 Error folder path MUST include module_name: "+ TEST_MDOULE_NAME);
+		assertEquals("conformance-topics/error_records/"+ TEST_MDOULE_NAME + "/" + conformanceTopic + "/", conformanceBaseErrorFolder);
+	}
+
+}
---
+++ Java/cdk-library/src/test/java/com/prod8ctive/linear/infro/GlueCdkTest.java
@@ -0,0 +1,98 @@
+package com.prod8ctive.infro;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.prod8ctive.infra.GlueCdk;
+import com.opencsv.exceptions.CsvValidationException;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.utils.Pair;
+
+/**
+ * 
+ * @author chawl001
+ */
+class GlueCdkTest {
+
+	String TEST_S3_BUCKET = "laap-ue1-gen-repository-sbx";
+	String TEST_TOPICS_FOLDER_WITH_DELIMETER = "avro_2_glue/output/cdk_test/";
+	
+	String TEST_LOCAL_SCHEMA_FOLDER = "src/main/resources/cdk_test/";
+	String TEST_GLUE_SCHEMA_FILENAME = "conformance_target_concommon__flght_rng.glue.csv";
+	
+	String TOPIC_NAME = 
+//			"lndcdc_ncs_tcs__flight_dates" 	/* -- GoldenGate topics */ 
+			"lndcdcncstcs__flightdates" 	/* -- NiFi topics */ ;
+	
+    static final S3Client s3 = S3Client.builder()
+            .region(Region.US_EAST_1)
+            .build();
+	
+	@Test
+	@Tag("integration-test")	// Needs file on real S3 with permissions setup
+	void testGetGlueSchemaS3() throws CsvValidationException, IOException {
+
+    	ListObjectsV2Request listObjects = ListObjectsV2Request
+                .builder()
+                .bucket(TEST_S3_BUCKET)
+                .prefix(TEST_TOPICS_FOLDER_WITH_DELIMETER)
+                .build();
+
+        ListObjectsV2Response res = s3.listObjectsV2(listObjects);
+        List<S3Object> objects = res.contents();
+        
+        assertNotNull(objects);
+        assertFalse(objects.isEmpty(), String.format("S3 folder s3://%s/%s has no Glue schema files", TEST_S3_BUCKET, TEST_TOPICS_FOLDER_WITH_DELIMETER));
+        System.out.println("S3 folder path: "+ TEST_TOPICS_FOLDER_WITH_DELIMETER +" has total "+ objects.size() +" glue schemas");
+
+        ListIterator<S3Object> iterVals = objects.listIterator();
+        if (iterVals.hasNext()) {
+        	S3Object glueSchemaCSV = iterVals.next();
+        	
+            System.out.println("Testing parsing of Glue Schema (CSV): " + glueSchemaCSV.key());
+            List<Pair<String,String>> columnsList = null;
+            try {
+            	columnsList = GlueCdk.getGlueSchemaFromS3(s3, TEST_S3_BUCKET, glueSchemaCSV.key());
+            } catch (RuntimeException e) {
+            	e.printStackTrace();
+            	fail("Failed with Exception: "+ e.getMessage());
+            }
+            assertNotNull(columnsList);
+            assertFalse(columnsList.isEmpty());
+            for (Pair<String,String> colInfo : columnsList) {
+            	System.out.println(colInfo);
+            }
+         }
+	}
+	
+	@Test
+	@Tag("integration-test")	// Needs file on local folder
+	void testGetGlueSchemaLocal() throws CsvValidationException, IOException {
+
+
+        List<Pair<String,String>> columnsList = null;
+        try {
+        	columnsList = GlueCdk.getGlueSchemaFromFolder(TEST_LOCAL_SCHEMA_FOLDER, TEST_GLUE_SCHEMA_FILENAME);
+        } catch (RuntimeException e) {
+        	e.printStackTrace();
+        	fail("Failed with Exception: "+ e.getMessage());
+        }
+        assertNotNull(columnsList);
+        assertFalse(columnsList.isEmpty());
+        for (Pair<String,String> colInfo : columnsList) {
+        	System.out.println(colInfo);
+        }
+	}
+
+}
